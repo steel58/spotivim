294be7fccf292655207cba615b179e856e47180d
Harper Nordin <harper.nordin@gmail.com>
2026-06-22 17:48:17 -0700

    gotta have all your files

---
+++ lua/spotivim/utils.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local function dump(tbl)
+    if type(tbl) == 'table' then
+        local s = '{ '
+        for k,v in pairs(tbl) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            s = s .. '['..k..'] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(tbl)
+    end
+end
+
+function M.print_table(tbl)
+    dump(tbl)
+end
+
+return M
